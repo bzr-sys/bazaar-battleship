490a10f002ac45eb9257c2a6d8ce6561697c822d
Sascha Trifunovic <sascha.trifunovic@gmail.com>
2022-09-16 16:36:58 +0200

    Only use local endpoints in development

--- src/rethinkid.ts
+++ src/rethinkid.ts
@@ -1,10 +1,11 @@
 import RethinkID from "@mostlytyped/rethinkid-js-sdk";
+import { Options } from "@mostlytyped/rethinkid-js-sdk/dist/types/types";
 
 const baseURL = window.location.origin;
 
 console.log("app id", process.env.VUE_APP_APP_ID);
 
-const config = {
+const config: Options = {
   appId: process.env.VUE_APP_APP_ID,
   loginRedirectUri: `${baseURL}`,
   dataAPIConnectErrorCallback: function () {
@@ -12,9 +13,11 @@ const config = {
     // @ts-ignore
     this.logOut();
   },
-
-  oAuthUri: "http://localhost:4444",
-  dataApiUri: "http://localhost:4000",
 };
+
+if (process.env.NODE_ENV == "development") {
+  config.oAuthUri = "http://localhost:4444";
+  config.dataApiUri = "http://localhost:4000";
+}
 
 export const rid = new RethinkID(config);
