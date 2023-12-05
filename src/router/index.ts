86abe273a7b165f67777cfd6ba6d507a489863a4
Sascha Trifunovic <sascha.trifunovic@gmail.com>
2023-12-05 08:55:15 +0100

    Use web hash history router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import GameView from "../views/GameView.vue";
 import { rid } from "@/rethinkid";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
